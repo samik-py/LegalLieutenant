bc52b6e71f04d2606211836ab351dd51538539b2
Samik Shah <samikmshah07@gmail.com>
2024-07-31 18:35:21 -0400

    server changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
-const express = require("express")
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
 
-app.get('/form/:formName', (req, res) => {
-  res.send(req.params.formName)
-})
+const upload = multer({ dest: 'uploads/' });
 
-app.listen(port, () => { })
 
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (req.file) {
+    console.log('File received:', req.file);
+    res.send('File uploaded successfully!');
+  } else {
+    res.status(400).send('No file uploaded');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
