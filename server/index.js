c6fd4628b4460a1f846584f0845018fcd74763b8
Samik Shah <samikmshah07@gmail.com>
2024-07-31 18:36:06 -0400

    server changes

--- server/index.js
+++ server/index.js
@@ -18,13 +18,13 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
-    console.log('File received:', req.file);
-    res.send('File uploaded successfully!');
+    console.log('file received:', req.file);
+    res.send('file uploaded successfully');
   } else {
     res.status(400).send('No file uploaded');
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
