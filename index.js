411e589a7a36f0d7d644ca2b93c2225cf0c63b34
ps-coding <95548088+ps-coding@users.noreply.github.com>
2024-07-31 22:21:51 -0400

    WIP - Form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require("express");
 const process = require("process");
+const path = require("path");
+const fs = require("fs")
+const express = require("express");
 const multer = require("multer");
+const reader = require("any-text");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,24 +11,40 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function(_, _, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(_, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-app.get("/", (req, res) => {
+const upload = multer({ storage });
+
+app.get("/", (_, res) => {
   res.render("pages/index");
 });
 
-app.get("/upload", (req, res) => {
-  res.render("pages/upload");
+app.get("/understand", (_, res) => {
+  res.render("pages/understand");
 });
 
-app.post("/upload-file", upload.single("file"), (req, res) => {
+app.get("/fill", (_, res) => {
+  res.render("pages/fill");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
   if (req.file) {
-    res.status(201).render("pages/index");
+    const documentContent = await reader.getText(req.file.path);
+    fs.unlink(req.file.path, (_) => { })
+    res.status(201).render("pages/explain", { documentContent });
   } else {
-    res.status(400).render("pages/upload");
+    res.redirect(400, "/");
   }
 });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
 });
+
