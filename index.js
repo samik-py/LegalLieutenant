689dc6d6a4a327017f34771ab5426fb9259f8777
ps-coding <95548088+ps-coding@users.noreply.github.com>
2024-08-01 23:51:33 -0400

    Done

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
+
 const process = require("process");
 const path = require("path");
 const fs = require("fs")
 const express = require("express");
 const multer = require("multer");
 const reader = require("any-text");
+const openai = require("openai");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env["PORT"] || 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }))
@@ -23,9 +26,42 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+const client = new openai.OpenAI({
+  apiKey: process.env['OPENAI_API_KEY']
+});
+
+const groupSections = (documentContent) => {
+  let splitSections = documentContent.replaceAll(/\s*((Article)|(Section)|(Preamble)|(Definitions)|(Clauses))\.?\s*((([0-9]+)|([IVXLCDM]+)|([A-Z]+))\.?)?\s*\r?\n/g, "[;break;]")
+  splitSections = splitSections.replaceAll("\n", " ").split("[;break;]");
+
+  if (splitSections.length == 1) {
+    splitSections = splitSections[0].replaceAll(/([\.\?!])/g, "$1\n").split("\n");
+
+    let result = [splitSections[0]];
+    for (let i = 1; i < splitSections.length; i++) {
+      const last = result[result.length - 1];
+      if (last.length < 800) {
+        result[result.length - 1] = last + splitSections[i];
+      } else {
+        result.push(splitSections[i]);
+      }
+    }
+
+    splitSections = result;
+  }
+
+  const groupedSections = splitSections.map((section) => section.trim()).filter((section) => section != "")
+
+  return groupedSections;
+}
+
 const summarize = async (section) => {
-  // TODO: Insert AI code here
-  return "summary"
+  const params = {
+    messages: [{ role: 'user', content: `Summarize the following section from a legal document in one simple sentence:\n${section}` }],
+    model: 'gpt-4o-mini',
+  };
+  const response = await client.chat.completions.create(params);
+  return response.choices[0].message.content;
 }
 
 app.get("/", (_, res) => {
@@ -43,8 +79,8 @@ app.get("/generate", (_, res) => {
 app.post("/explain-file", upload.single("file"), async (req, res) => {
   if (req.file) {
     const documentContent = await reader.getText(req.file.path);
-    const all_sections = await Promise.all(documentContent.split("\n").map(async (section) => ({ content: section, summary: await summarize(section) })));
-    const sections = all_sections.filter((section) => section.content.trim() != "")
+    const groupedSections = groupSections(documentContent);
+    const sections = await Promise.all(groupedSections.map(async (section) => ({ content: section, summary: await summarize(section) })));
     fs.unlink(req.file.path, (err) => {
       if (err) console.error(err)
     })
@@ -56,8 +92,8 @@ app.post("/explain-file", upload.single("file"), async (req, res) => {
 
 app.post("/explain-text", async (req, res) => {
   const documentContent = req.body.documentContent;
-  const all_sections = await Promise.all(documentContent.split("\n").map(async (section) => ({ content: section, summary: await summarize(section) })));
-  const sections = all_sections.filter((section) => section.content.trim() != "")
+  const groupedSections = groupSections(documentContent);
+  const sections = await Promise.all(groupedSections.map(async (section) => ({ content: section, summary: await summarize(section) })));
   res.status(201).render("pages/explain-results", { sections });
 });
 
